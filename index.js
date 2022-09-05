1724898cd0eb623149550d7b697f4c4d0c5eee6b
Hrak Torousian <hraktorres@gmail.com>
2022-09-05 09:58:02 -0700

    add connection

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected");
+  } catch (err) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnect", () => {
+  console.log("mongodb disconnected");
+});
+
+mongoose.connection.on("connect", () => {
+  console.log("mongodb connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(8800, () => {
-  console.log("connected");
+  connect();
+  console.log("connected to back");
 });
